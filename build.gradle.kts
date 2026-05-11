91e4fd105d9464294731376334dc401ef34f078a
Oscar Duarte <leodufer@gmail.com>
2026-05-11 20:27:52 -0300

    Integrate SonarQube for code quality analysis
    Add SonarQube plugin and configuration for project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    id("org.sonarqube") version "7.2.3.7755"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "ExpensesTracker")
+    property("sonar.projectName", "ExpensesTracker")
+  }
 }
